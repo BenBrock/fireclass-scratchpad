a23ec741755529ea696c074c8f48644468a3fa80
Benjamin Brock <brock@cs.berkeley.edu>
2017-11-01 17:44:38 -0700

    Testing compaction

---
+++ example/malloc_compaction.cpp
@@ -0,0 +1,42 @@
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+
+extern "C" {
+  #include <remote_scratchpad.h>
+  void print_mem_list(void *scratchpad);
+}
+
+int main(int argc, char **argv) {
+  size_t n = 65536;
+  void *scratchpad = create_scratchpad(n);
+  init_scratchpad_malloc(scratchpad);
+
+  int *a = (int *) scratch_malloc(scratchpad, 128);
+  int *b = (int *) scratch_malloc(scratchpad, 128);
+  int *c = (int *) scratch_malloc(scratchpad, 128);
+
+  print_mem_list(scratchpad);
+  scratch_free(scratchpad, a);
+  print_mem_list(scratchpad);
+  scratch_free(scratchpad, b);
+  print_mem_list(scratchpad);
+
+  int *d = (int *) scratch_malloc(scratchpad, 256);
+  print_mem_list(scratchpad);
+
+
+  int *e = (int *) scratch_malloc(scratchpad, 128);
+  int *f = (int *) scratch_malloc(scratchpad, 128);
+  int *g = (int *) scratch_malloc(scratchpad, 128);
+
+  print_mem_list(scratchpad);
+  scratch_free(scratchpad, f);
+  print_mem_list(scratchpad);
+  scratch_free(scratchpad, e);
+  print_mem_list(scratchpad);
+
+  destroy_scratchpad(scratchpad);
+
+  return 0;
+}
