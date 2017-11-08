aa386eb105a42e35eb2a1f8dd9629b8f84ab91ec
Benjamin Brock <brock@cs.berkeley.edu>
2017-11-07 20:49:53 -0800

    CachedRead working well :D

--- benchmarks/genome-assembly/serial-memblade/assemble.cpp
+++ benchmarks/genome-assembly/serial-memblade/assemble.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <numeric>
 #include <memory>
+#include <chrono>
 #include "kmer_t.hpp"
 #include "read_kmers.hpp"
 
@@ -15,9 +16,10 @@ extern "C" {
 #include "PackedWrite.hpp"
 
 int main(int argc, char **argv) {
-  std::vector <kmer_t> kmers = read_kmers("tiny.dat");
+  std::vector <kmer_t> kmers = read_kmers("verysmall.dat");
   printf("%d kmers read.\n", kmers.size());
 
+  auto begin = std::chrono::high_resolution_clock::now();
   size_t block_size = 4096;
   size_t n_blocks = 1000000;
   size_t size_of_kmer;
@@ -41,25 +43,32 @@ int main(int argc, char **argv) {
   }
   write.issue();
 
-  printf("Made hash table!\n");
-  kmers.clear();
+  auto after_write = std::chrono::high_resolution_clock::now();
+
+  printf("Written to distributed hash table on memory blade.\n");
 
   std::list <std::list <kmer_t>> contigs;
 
-  CachedRead read(block_size, n_blocks);
+  CachedRead read(block_size, n_blocks, 100);
 
   for (const auto &kmer : start_nodes) {
     std::list <kmer_t> contig;
     contig.push_back(kmer);
+    std::string packed_kmer(size_of_kmer, 'a');
     while (contig.back().forwardExt() != 'F') {
       size_t loc = kmer_hash[contig.back().nextKmer()];
-      std::string packed_kmer(size_of_kmer, 'a');
       read.read(loc, packed_kmer.size(), packed_kmer.data());
       kmer_t my_kmer = unpack_kmer(packed_kmer);
       contig.push_back(my_kmer);
     }
     contigs.push_back(contig);
   }
+  auto end = std::chrono::high_resolution_clock::now();
+  std::chrono::duration <double> totalTime = end-begin;
+  std::chrono::duration <double> writeTime = after_write - begin;
+  std::chrono::duration <double> readTime = end - after_write;
+  printf("Completed assembly in %lf seconds (%lf write, %lf read)\n", totalTime.count(),
+    writeTime.count(), readTime.count());
 
   int numKmers = std::accumulate(contigs.begin(), contigs.end(), 0,
     [] (int sum, const std::list <kmer_t> &contig) {
--- memblade-user/CachedRead.hpp
+++ memblade-user/CachedRead.hpp
@@ -5,17 +5,31 @@ extern "C" {
 }
 
 #include <cstring>
+#include <memory>
+#include <map>
+#include <algorithm>
+
+class Page {
+public:
+  std::shared_ptr <char> data;
+  uint64_t last_used;
+
+  Page(std::shared_ptr <char> data, uint64_t last_used = 0) :
+    data(data), last_used(last_used) {}
+  Page() {}
+};
 
 class CachedRead {
 public:
   size_t n_blocks, block_size;
   size_t begin_address = 0;
-  size_t size_bytes;
+  size_t size_bytes, cache_size;
+  uint64_t mem_clock = 0;
 
-  std::map <int, std::shared_ptr <char>> pages;
+  std::map <int, Page> pages;
 
-  CachedRead(size_t block_size, size_t n_blocks) :
-    n_blocks(n_blocks), block_size(block_size) {
+  CachedRead(size_t block_size, size_t n_blocks, size_t cache_size = 0) :
+    n_blocks(n_blocks), block_size(block_size), cache_size(cache_size) {
       size_bytes = begin_address + n_blocks*block_size;
   }
 
@@ -25,19 +39,42 @@ public:
       size_t block = cur_addr / block_size;
       size_t block_end_addr = (block+1) * block_size;
       size_t cur_size = std::min(block_end_addr - cur_addr, end_addr - cur_addr);
-      if (cur_size == block_size) {
-        remote_get(block, (char *) dst + (cur_addr - addr), 1);
-      } else {
-        std::shared_ptr <char> data(new char[block_size]);
-        remote_get(block, data.get(), 1);
-        memcpy((char *) dst + (cur_addr - addr),
-          (char *) data.get() + (cur_addr - block*block_size), cur_size);
+      std::shared_ptr <char> data = get_block(block);
+      memcpy((char *) dst + (cur_addr - addr),
+        (char *) data.get() + (cur_addr - block*block_size), cur_size);
+    }
+  }
+
+  std::shared_ptr <char> get_block(size_t block) {
+    // If cache turned off, just get page.
+    if (cache_size == 0) {
+      std::shared_ptr <char> data(new char[block_size]);
+      remote_get(block, data.get(), 1);
+      return data;
+    }
+
+    if (pages.find(block) == pages.end()) {
+      if (pages.size() + 1 > cache_size) {
+        remove_lru();
       }
+      pages[block] = Page(std::shared_ptr <char> (new char[block_size]));
+      remote_get(block, pages[block].data.get(), 1);
+    }
+    pages[block].last_used = mem_clock++;
+    return pages[block].data;
+  }
+
+  void remove_lru() {
+    auto lru = std::min_element(pages.begin(), pages.end(),
+      [] (std::pair <int, Page> x, std::pair <int, Page> y) {
+        return x.second.last_used < y.second.last_used;
+      });
+    if (lru != pages.end()) {
+      pages.erase(lru);
     }
   }
 
   void flush() {
     pages.clear();
   }
-
 };
